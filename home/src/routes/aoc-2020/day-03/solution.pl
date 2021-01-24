267c41418fc63b6f1ea8329e9f9592ebf45058c6
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-01-23 16:45:12 -0800

    Add final solution for day 3

--- home/src/routes/aoc-2020/day-03/solution.pl
+++ home/src/routes/aoc-2020/day-03/solution.pl
@@ -35,28 +35,36 @@ main(Input, Result):-
     split_string(Input, '\n', '\n', L),
     isTree(L, 0, 0, Result).
 
-atPos2(String, Counter, Right, Down, Char):-
+atPos2(String, Counter, Right, Char):-
     string_chars(String, Chars),
     length(Chars, Modulus),
     % make sure to take into account the slope
-    Index is mod(Counter/Down*Right, Modulus),
+    Index is mod(Counter*Right, Modulus),
     nth0(Index, Chars, Char).
+
+% This is necessary to traverse down the slope correclty
+skipList(L, 0, L).
+skipList([_], _, []).
+skipList([_|Rest], Counter, Result):-
+    C1 is Counter - 1,
+    skipList(Rest, C1, Result).
 
 isTree2([], _,_,_, X, X).
 isTree2([X|Xs], Right, Down, Counter, Acc, Result):-
-    atPos2(X, Counter, Right, Down, Char),
-    C1 is Counter + Down,
+    atPos2(X, Counter, Right, Char),
+    skipList([X|Xs], Down, Xss),
+    C1 is Counter + 1,
     (   Char = '#' ->
     	A1 is Acc + 1;
     	A1 is Acc
     ),
-    isTree2(Xs, Right, Down, C1, A1, Result).
+    isTree2(Xss, Right, Down, C1, A1, Result).
 
 main2(Input, Result):-
     split_string(Input, '\n', '\n', L),
-    isTree2(L, 1, 2, 0, 0, E),
     isTree2(L, 1, 1, 0, 0, A),
     isTree2(L, 3, 1, 0, 0, B),
     isTree2(L, 5, 1, 0, 0, C),
     isTree2(L, 7, 1, 0, 0, D),
+    isTree2(L, 1, 2, 0, 0, E),
     Result is A*B*C*D*E.
