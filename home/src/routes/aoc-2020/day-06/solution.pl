194c13d72e97be798ff1e562322997447e63eae7
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-01-26 17:38:54 -0800

    Add correct solution for day 6 part 2

--- home/src/routes/aoc-2020/day-06/solution.pl
+++ home/src/routes/aoc-2020/day-06/solution.pl
@@ -13,23 +13,26 @@ main(Input, Output):-
     sumlist(Counts, Output).
 
 % the same as process, except an intersection instead of union
-process2([], X, [X]).
-process2([""|T], X, [X|Res]):-
-    process2(T, [], Res).
-% however, it needs an extra rule when the intitial set is null
-process2([H|T], [], Res):-
+% however, it needs an extra rule when the intitial set is null, and
+% we need to differentiate between the first and last intersection in a set
+process2([], _, X, [X]).
+process2([H|T], 0, [], Res):-
     string_chars(H, Items),
-    process2(T, Items, Res).
-process2([H|T], Acc, Res):-
+    process2(T, 1, Items, Res).
+process2([""|T], _, X, [X|Res]):-
+    process2(T, 0, [], Res).
+process2([H|T], C, Acc, Res):-
     string_chars(H, Items),
     intersection(Items, Acc, Acc2),
-    process2(T, Acc2, Res).
+    C2 is C + 1,
+    process2(T, C2, Acc2, Res).
 
 main2(Input, Output):-
     split_string(Input, '\n', '\s', Lines),
-    process2(Lines,[], Groups),
+    process2(Lines, 0, [], Groups),
     maplist(length, Groups, Counts),
     sumlist(Counts, Output).
+
 
 sample("abc
 
