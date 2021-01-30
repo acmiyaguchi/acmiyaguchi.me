677ff624fa61a4909e9839ee2fab0a188a0a164e
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-01-29 23:42:40 -0800

    Fix issues with component reusing preload data

--- home/src/routes/api/v1/blog-posts.json.js
+++ home/src/routes/api/v1/blog-posts.json.js
@@ -1,12 +1,8 @@
 import posts from "../../blog/_posts/*";
 import path from "path";
-import url from "url";
-import querystring from "querystring";
 import { sortBy } from "lodash";
 
 export function get(req, res, next) {
-  let parsed = url.parse(req.url);
-  let query = querystring.parse(parsed.query);
   let results = sortBy(
     posts.map((p) => {
       let metadata = p.exports.metadata;
@@ -16,7 +12,5 @@ export function get(req, res, next) {
     }),
     ["date"]
   ).reverse();
-  res.end(
-    JSON.stringify(query.limit ? results.slice(0, query.limit) : results)
-  );
+  res.end(JSON.stringify(results));
 }
