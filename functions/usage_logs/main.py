feb6d554b09c6382db6002d21b9f7001d3c23a75
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-01-30 12:30:30 -0800

    Add function for dumping usage logs

---
+++ functions/usage_logs/main.py
@@ -0,0 +1,61 @@
+import gzip
+import json
+from datetime import date, datetime
+
+from google.cloud import bigquery, storage
+
+
+def default_serializer(obj):
+    """JSON serializer for objects that cannot be serialized by default json code
+
+    https://stackoverflow.com/a/22238613
+    """
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError(f"Type {type(obj)} cannot be serialized")
+
+
+def dump_to_gcs(bucket, name, query):
+    """Upload data to a gcs bucket in a compressed format."""
+    bq = bigquery.Client()
+    gcs = storage.Client()
+    bucket = gcs.bucket(bucket)
+    blob = bucket.blob(
+        f"v1/query/{name}.json",
+    )
+    blob.content_encoding = "gzip"
+    # always serve compressed content
+    blob.cache_control = "no-transform"
+    job = bq.query(query)
+    print(f"running query {job.job_id} for {name}: {query}")
+    output = [dict(row) for row in job]
+    # https://cloud.google.com/storage/docs/transcoding#decompressive_transcoding
+    blob.upload_from_string(
+        gzip.compress(json.dumps(output, default=default_serializer).encode()),
+        content_type="application/json",
+    )
+
+
+def usage_logs(request):
+    bq = bigquery.Client()
+    job_config = bigquery.LoadJobConfig(
+        autodetect=True,
+        source_format=bigquery.SourceFormat.CSV,
+        # just always load the entire bucket into BigQuery
+        write_disposition=bigquery.job.WriteDisposition.WRITE_TRUNCATE,
+    )
+    load_job = bq.load_table_from_uri(
+        "gs://acmiyaguchi-logs/acmiyaguchi_usage*",
+        "acmiyaguchi.logs.acmiyaguchi_usage",
+        job_config=job_config,
+    )
+    print(f"running load job {load_job.job_id}")
+    load_job.result()
+
+    dump_to_gcs("acmiyaguchi", "logs_page_visits", "select * from logs.page_visits")
+
+    return "OK"
+
+
+if __name__ == "__main__":
+    usage_logs(None)
