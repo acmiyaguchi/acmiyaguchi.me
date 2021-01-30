6e0d7e8f05aa4b2aab566410369b4dfd1901ced1
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-01-29 20:38:01 -0800

    Add proper timestamps to posts and format using dayjs

--- home/src/routes/api/v1/blog-posts.json.js
+++ home/src/routes/api/v1/blog-posts.json.js
@@ -1,15 +1,19 @@
 import posts from "../../blog/_posts/*";
 import path from "path";
+import { sortBy } from "lodash";
 
 export function get(req, res, next) {
   res.end(
     JSON.stringify(
-      posts.map((p) => {
-        let metadata = p.exports.metadata;
-        // inject the path name into the metadata
-        metadata.name = path.basename(p.path).split(".svx")[0];
-        return metadata;
-      })
+      sortBy(
+        posts.map((p) => {
+          let metadata = p.exports.metadata;
+          // inject the path name into the metadata
+          metadata.name = path.basename(p.path).split(".svx")[0];
+          return metadata;
+        }),
+        ["date"]
+      ).reverse()
     )
   );
 }
