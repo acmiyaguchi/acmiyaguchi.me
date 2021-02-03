9894a8f331be042ddf6a6fc5fa2ab78216eff182
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-02-02 21:31:34 -0800

    Add toastmasters intro (series kickoff)

--- home/src/routes/toastmasters/index.svelte
+++ home/src/routes/toastmasters/index.svelte
@@ -1,5 +1,14 @@
-<script>
-  import Body from "./Body.svx";
+<script context="module">
+  export async function preload() {
+    let resp = await this.fetch("api/v1/blog-posts.json");
+    let posts = await resp.json();
+    return { metadata: posts.filter(p => p.category == "Toastmasters") };
+  }
 </script>
 
-<Body />
+<script>
+  import Body from "./Body.svx";
+  export let metadata;
+</script>
+
+<Body {metadata} />
