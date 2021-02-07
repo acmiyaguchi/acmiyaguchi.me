90d2ef269ff62dba298eb11504815a618fd83b90
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-02-07 12:57:30 -0800

    Fix routes on piano to not have mid at the end

--- home/src/routes/piano/index.svelte
+++ home/src/routes/piano/index.svelte
@@ -3,7 +3,14 @@
     let url = `https://storage.googleapis.com/acmiyaguchi/midi/manifest.json`;
     let resp = await this.fetch(url);
     let data = await resp.json();
-    return { manifest: data.filter(row => row.name.endsWith(".mid")) };
+    return {
+      manifest: data
+        .filter(row => row.name.endsWith(".mid"))
+        .map(row => ({
+          slug: row.name.split(".mid")[0],
+          ...row
+        }))
+    };
   }
 </script>
 
