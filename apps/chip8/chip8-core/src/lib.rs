71bbe8e1dc22295a29f83ca3c85f677aec5c6e10
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-02-28 01:27:32 -0800

    Add view into the display memory

--- apps/chip8/chip8-core/src/lib.rs
+++ apps/chip8/chip8-core/src/lib.rs
@@ -1,3 +1,4 @@
+use js_sys;
 use std::f64;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -10,57 +11,18 @@ use cpu::Cpu;
 
 #[wasm_bindgen]
 pub struct Core {
-    cpu: Cpu
+    cpu: Cpu,
 }
 
+#[wasm_bindgen]
 impl Core {
-    pub fn init() -> Self {
+    pub fn new() -> Self {
         Self {
             cpu: Cpu::default(),
         }
     }
-}
 
-// https://rustwasm.github.io/wasm-bindgen/examples/2d-canvas.html
-#[wasm_bindgen(start)]
-pub fn start() {
-    utils::set_panic_hook();
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .unwrap();
-
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
-
-    context.begin_path();
-
-    // Draw the outer circle.
-    context
-        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
-
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    context.stroke();
+    pub fn get_display(self) -> js_sys::Uint8Array {
+        js_sys::Uint8Array::from(self.cpu.display.memory.as_ref())
+    }
 }
