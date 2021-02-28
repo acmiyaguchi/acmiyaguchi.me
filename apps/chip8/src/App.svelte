9506df4cfb296bd596e4fd5b1fa409ac5ad0f438
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-02-27 23:42:59 -0800

    Add demo web-sys to the project

--- apps/chip8/src/App.svelte
+++ apps/chip8/src/App.svelte
@@ -1,8 +1,22 @@
 <script>
+  import { onMount } from "svelte";
+  import wasm from "../chip8-core/Cargo.toml";
+
+  export let Chip8Core;
+
+  onMount(async () => {
+    Chip8Core = await wasm();
+    Chip8Core.start();
+  });
 </script>
 
+<svelte:head>
+  <title>chip8 wasm demo</title>
+</svelte:head>
+
 <main>
-  <h1>Hello world!</h1>
+  <h1>chip8 wasm demo</h1>
+  <canvas id="canvas" />
 </main>
 
 <style>
