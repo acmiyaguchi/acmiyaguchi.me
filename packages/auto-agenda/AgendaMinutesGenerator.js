29de20dd2eb99ff3c5c7a19d14819d12cf9f82d2
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-03-06 20:16:24 -0800

    Remove old files before creating new ones

--- packages/auto-agenda/AgendaMinutesGenerator.js
+++ packages/auto-agenda/AgendaMinutesGenerator.js
@@ -8,6 +8,8 @@ const AMG_MINUTES_TEMPLATE_ID = "11NdRpVHwGTBLiCoCQmr8sDlskHnfkjzMMrhnB5iPpU8";
 const AMG_AGENDA_OUTPUT_FOLDER_ID = "14fBhsQ7u34EtXVWS8lWLS7QBwK7cdLNA";
 const AMG_MINUTES_OUTPUT_FOLDER_ID = "1GWySjq8y4OQS48PHT2vRyTxXWxKv8PyL";
 
+const WEEKS_TO_GENERATE = 3;
+
 function onOpen() {
   let menuEntries = [
     {
@@ -19,6 +21,40 @@ function onOpen() {
   ss.addMenu("Create", menuEntries);
 }
 
+function generateAgenda() {
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  let sheet = ss.getSheetByName("Roles");
+
+  // generate from the N most recent documents
+  let cutoff = WEEKS_TO_GENERATE;
+  let data = fetchSpreadsheetData(sheet).slice(0, 3);
+  Logger.log(JSON.stringify(data[0], " ", 2));
+  fillTemplate(
+    data.slice(0, 3),
+    AMG_AGENDA_OUTPUT_FOLDER_ID,
+    AMG_AGENDA_TEMPLATE_ID,
+    (row) => `MVTM Meeting Agenda, ${row.DATE.toISOString().slice(0, 10)}`
+  );
+  ss.toast("Agendas have been compiled!");
+}
+
+function generateMinutes() {
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  let sheet = ss.getSheetByName("Roles");
+
+  // generate from the N most recent documents
+  let cutoff = WEEKS_TO_GENERATE;
+  let data = fetchSpreadsheetData(sheet).slice(0, 3);
+  Logger.log(JSON.stringify(data[0], " ", 2));
+  fillTemplate(
+    data.slice(0, 3),
+    AMG_MINUTES_OUTPUT_FOLDER_ID,
+    AMG_MINUTES_TEMPLATE_ID,
+    (row) => `Meeting Minutes, ${row.DATE.toISOString().slice(0, 10)}`
+  );
+  ss.toast("Minutes have been compiled!");
+}
+
 function normalizeName(name) {
   return name
     .toUpperCase()
@@ -28,7 +64,8 @@ function normalizeName(name) {
     .join("_");
 }
 
-// Convert spreadsheet data from a sheet object (csv) into an array of objects
+// Convert spreadsheet data from a sheet object (csv) into an array of objects in
+// reverse chronological order
 function fetchSpreadsheetData(sheet) {
   let values = sheet
     .getRange(1, 1, sheet.getLastRow(), sheet.getLastColumn())
@@ -53,20 +90,14 @@ function fetchSpreadsheetData(sheet) {
   return entries;
 }
 
-function generateMinutes() {
-  let ss = SpreadsheetApp.getActiveSpreadsheet();
-  let sheet = ss.getSheetByName("Roles");
-
-  // generates the most recent document
-  let data = fetchSpreadsheetData(sheet);
-  Logger.log(JSON.stringify(data[0], " ", 2));
-  fillTemplate(
-    data.slice(0, 3),
-    AMG_MINUTES_OUTPUT_FOLDER_ID,
-    AMG_MINUTES_TEMPLATE_ID,
-    (row) => `Meeting Minutes, ${row.DATE.toISOString().slice(0, 10)}`
-  );
-  ss.toast("Templates have been compiled!");
+// The iterator doesn't actually implement a javascript iterator interface; instead
+// read everything into an array. Don't use this to read the entire drive...
+function intoArray(gsIterator) {
+  let result = [];
+  while (gsIterator.hasNext()) {
+    result.push(gsIterator.next());
+  }
+  return result;
 }
 
 function fillTemplate(
@@ -80,6 +111,12 @@ function fillTemplate(
   for (row of data) {
     let name = title_formatter(row);
     Logger.log(`creating document for ${name}`);
+    // trash the old document with the same name
+    for (let file of intoArray(folder.getFilesByName(name))) {
+      Logger.log(`trashing existing file ${name}`);
+      file.setTrashed(true);
+    }
+
     let docId = DriveApp.getFileById(template_id)
       .makeCopy(name, folder)
       .getId();
